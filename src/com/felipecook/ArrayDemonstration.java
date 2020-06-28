4ccd5cbf4157154663da157439a6c37776065db7
Felipe Cook <felipepcook@gmail.com>
2020-06-28 14:59:58 -0600

    tutor session 3

---
+++ src/com/felipecook/ArrayDemonstration.java
@@ -0,0 +1,28 @@
+package com.felipecook;
+
+public class ArrayDemonstration {
+
+  int[] simpleArray = {1, 2, 3, 4, 5};
+
+  int valueOfFirstElement = simpleArray[0];
+
+
+  int[][] multiArray = new int[1][2];
+
+  /*                    xxxxx
+  *
+  *                       xx
+  *                       xx
+  *
+  *
+  *                     xxx
+  *                     xx
+  *                     x
+  *
+  * */
+
+  
+
+
+
+}
