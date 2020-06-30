b22baaf77526249bf99e0560af576e57e4e9f875
Felipe Cook <felipepcook@gmail.com>
2020-06-29 19:19:49 -0600

    tutor session 5

--- src/com/felipecook/PracticeProblems.java
+++ src/com/felipecook/PracticeProblems.java
@@ -1,25 +1,53 @@
 package com.felipecook;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.stream.events.Characters;
+
 public class PracticeProblems {
+
+
+  /*
+  * Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are
+  * removed, but the "a" can be any char. The "yak" strings will not overlap.
+
+    stringYak("yakpak") → "pak"
+    stringYak("pakyak") → "pak"
+    stringYak("yak123ya") → "123ya"
+
+*
+*         "yakpak" -> {'y','a','k','p','a','k'}
+*
+*       ['p', 'a', 'k'] -> {'p','a','k'} -> "pak"
+*
+*
+  * */
+  public String stringYak(String str) {
+
+    // turns the string into a char array which is easier to manipulate
+    char[] strAsCharArray = str.toCharArray();
+
+    List<Character> modifiedStrAsList = new ArrayList<>();
+
+    for (int i = 0; i < str.length() - 2; i++) {
+
+      char singleElement = strAsCharArray[i];
+
+      if (singleElement == 'y' && strAsCharArray[i + 1] == 'a' && strAsCharArray[i + 2] == 'k') {
+        i = i + 3;
+      } else {
+        modifiedStrAsList.add(singleElement);
+      }
+
+
+    }
+
+  }
 
 
   public static void main(String[] args) {
 
-    Car felipesCar = new Car();
-
-    System.out.println("The current make of the car is: " + felipesCar.getMake());
-
-    felipesCar.setMake("Toyota");
-
-    System.out.println("Now the make of the car is: " + felipesCar.getMake());
-
-    felipesCar.drive();
-
-    Ford anasCar = new Ford();
-
-    System.out.println("The make of this car is currently: " + anasCar.getMake());
-
-    anasCar.drive();
 
   }
 
